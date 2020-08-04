73f0bc282a66b0ec9990625c334651247084c3ad
Bárbara N. Sousa <barbara.ns@outlook.com>
2020-08-04 12:57:56 -0300

    Adicionando desafio 02 do Node.JS

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,48 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomes = this.transactions.map((transaction): number => {
+      if (transaction.type === 'income') {
+        return transaction.value;
+      }
+      return 0;
+    });
+    const somaIncomes = incomes.reduce(
+      (total: number, values: number): number => total + values,
+      0,
+    );
+
+    const outcomes = this.transactions.map((transaction): number => {
+      if (transaction.type === 'outcome') {
+        return transaction.value;
+      }
+      return 0;
+    });
+
+    const somaOutcomes = outcomes.reduce(
+      (total: number, values: number): number => total + values,
+      0,
+    );
+
+    const balance = {
+      income: somaIncomes,
+      outcome: somaOutcomes,
+      total: somaIncomes - somaOutcomes,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
